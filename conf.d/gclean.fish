9563dcad1c7e851e5f34b3d42432573ddc329cb7
noah harvey <noah.harvey@generalassemb.ly>
2019-04-24 14:43:54 -0400

    changes gclean to only remove merged branches

--- conf.d/gclean.fish
+++ conf.d/gclean.fish
@@ -1,13 +1,3 @@
 function gclean -d "cleans remote tracking branches"
-  git fetch upstream
-  git fetch origin
-
-  git checkout master
-  git merge upstream/master
-
-  git remote prune upstream
-    and git branch -vv | grep 'upstream/.*: gone]' | awk '{print $1}' | xargs git branch -d
-
-  git remote prune origin
-    and git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git branch -d
+  git branch --merged master | grep -v master | xargs -L 1 git branch -d
 end
